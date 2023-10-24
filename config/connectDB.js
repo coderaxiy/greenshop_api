a1ebc81c1c84290fe4122ea3812b24e9344f0197
kholiylullohahmadjanov <codevme@gmail.com>
2023-10-24 21:19:34 +0500

    sql fix

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,7 +4,13 @@ const pool = mysql.createConnection({
     host:'13.53.182.108',
     user:'root',
     password:'99Y01m18D',
-    database:'greenshop'
+    database:'greenshop',
+    connectTimeout: 86400000,
+    acquireTimeout: 86400000,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
-module.exports = {pool}
+
+module.exports = { pool }
